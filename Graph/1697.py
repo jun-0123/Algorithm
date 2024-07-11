969d84a06a5dea454b5475a1423cd9120760c379
junyeong kim <Tnrdlswjfal@gmail.com>
2024-07-11 13:45:57 +0900

    숨바꼭질

---
+++ Graph/1697.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+sec = 0
+q = deque()
+q.append([N, 0])
+# dic = {}
+visit = set()
+v = N
+
+while v != K:
+    # print(q)
+    v, sec = map(int, q.popleft())
+    visit.add(v)
+    # print(visit)
+    # if v not in dic:
+    #     dic[v] = [v * 2, v + 1, v - 1]
+    # print(dic)
+
+    for i in [v * 2, v + 1, v - 1] if v < K else [v - 1]:
+        if 0 <= i <= 100000 and i not in visit:
+            q.append([i, sec + 1])
+print(sec)
