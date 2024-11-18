56032b99083d8f98ce4bd5ba8aa6ffa668f331c4
jun-0123 <144248752+jun-0123@users.noreply.github.com>
2024-11-18 09:11:13 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,22 @@
+WITH RECURSIVE Hierarchy as(
+    
+select id, parent_Id, 1 as GENERATION
+from ECOLI_DATA
+where PARENT_ID is null
+    
+union all
+    
+select e.id, e.parent_Id, h.GENERATION +1 as GENERATION
+from ECOLI_DATA e join Hierarchy h on e.parent_Id=h.id
+    
+)
+
+select count(*) as COUNT,GENERATION
+from Hierarchy h join ECOLI_DATA e on h.id=e.id
+where e.id not in (SELECT distinct(parent_Id) 
+                   from ECOLI_DATA where parent_Id is not null)
+group by GENERATION
+
+
+
+
