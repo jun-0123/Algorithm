79952adf48c2fc141ff3f9ff80580f44feca9439
jun-0123 <144248752+jun-0123@users.noreply.github.com>
2025-03-04 16:04:54 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 14.25 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        int idx = overwrite_string.length();
+        return my_string.substring(0,s)+overwrite_string+my_string.substring(idx+s);
+        
+    }
+}
